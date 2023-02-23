43e6538ac0e3e36752dea0f26e376d8a8d68b073
Jaxon Walter <jaxonskywalter@gmail.com>
2023-02-23 11:20:03 -0700

    test2

--- Mission8/Startup.cs
+++ Mission8/Startup.cs
@@ -52,7 +52,7 @@ namespace Mission8
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id}");
             });
         }
     }
