55bbb0e07fe1efd2645fd63d0174bdcb7811ff7c
Jaxon Walter <jaxonskywalter@gmail.com>
2023-02-24 09:22:26 -0700

    added quadrant model

---
+++ Mission8/Models/Quadrant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission8.Models
+{
+    public class Quadrant
+    {
+        [Key]
+        [Required]
+        public int QuadrantId { get; set; }
+
+        public string QuadrantName { get; set; }
+    }
+}
