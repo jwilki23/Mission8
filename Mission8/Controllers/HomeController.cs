7f5f8a3ff1162c94654682c5be59005d8bb533bd
jwilki23 <wilkinsonjacob22@gmail.com>
2023-02-24 09:10:31 -0700

    controller

--- Mission8/Controllers/HomeController.cs
+++ Mission8/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Mission8.Models;
 using System;
@@ -11,27 +12,99 @@ namespace Mission8.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private AddTaskContext taskContext { get; set; }
+        //Constructor
+        public HomeController(AddTaskContext task)
         {
-            _logger = logger;
+            taskContext = task;
         }
-
+        //method for when you click on index
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        //method for when retrieving new entry page
+        [HttpGet]
+        public IActionResult AddTask()
         {
+            //pulls category data
+            ViewBag.Categories = taskContext.Categories.ToList();
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        //method to add task to data base
+        [HttpPost]
+        public IActionResult AddTask(TaskResponse tr)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            ViewBag.Categories = taskContext.Categories.ToList();
+            //method to add and save changes to entry if valid data is entered
+            if (ModelState.IsValid)
+            {
+                taskContext.Add(tr);
+                taskContext.SaveChanges();
+                return View("Index", tr);
+            }
+            //otherwise returns them back to the page they were on
+            else
+            {
+                return View(tr);
+            }
+
+        }
+        
+
+        public IActionResult Collection()
+        {
+            var entries = taskContext.Responses
+                .Include(x => x.Quadrant)
+                .OrderBy(x => x.Category)
+                .ToList();
+            return View(entries);
+        }
+        //method to edit an entry
+        [HttpGet]
+        public IActionResult Edit(int taskid)
+        {
+            //pulls category data
+            ViewBag.Categories = taskContext.Categories.ToList();
+            //creates variable holding data for the specified entry
+            var response = taskContext.Responses.Single(x => x.TaskId == taskid);
+            //returns NewMovies view with data from specified entry
+            return View("AddTask", response);
+        }
+        //Saves the edits to the database
+        [HttpPost]
+        public IActionResult Edit(TaskResponse blah)
+        {
+            //method to add and save changes to entry if valid data is entered
+            if (ModelState.IsValid)
+            {
+                taskContext.Add(blah);
+                taskContext.SaveChanges();
+                return View("Tasks", blah);
+            }
+            //otherwise returns them back to the page they were on
+            else
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int taskid)
+        {
+            //creates variable holding data for the specified entry
+            var response = taskContext.Responses.Single(x => x.TaskId == taskid);
+            //returns delete view with data from specified entry
+            return View(response);
+        }
+        [HttpPost]
+        public IActionResult Delete(TaskResponse tr)
+        {
+            //method to delete and save changes to entry if valid data is entered
+            taskContext.Responses.Remove(tr);
+            taskContext.SaveChanges();
+            //redirects back to collection view
+            return RedirectToAction("Tasks");
         }
     }
 }
